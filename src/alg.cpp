ae46bcee977726e49fee8fefe516be21b308b3f9
anton-mp <abramova2712katya@gmail.com>
2026-02-24 19:09:15 +0300

    Update spaces in alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,26 +4,21 @@
 
 
 bool checkPrime(uint64_t value) {
-    if (value == 1)
-    {
+  if (value == 1) {
 	    return false;
+  }
+  for (int i = 2; i <= sqrt(value); i++) {
+    if (value % i == 0) {
+      return false;
     }
-    for (int i = 2; i <= sqrt(value); i++)
-    {
-        if (value % i == 0)
-        {
-            return false;
-        }
-    }
-    return true;
+  }
+  return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-	int count = 0, i = 2;
-	while (count != n)
-	{
-		if (checkPrime(i))
-		{
+	uint64_t count = 0, i = 2;
+	while (count != n) {
+		if (checkPrime(i)) {
 			count++;
 		}
 		i++;
@@ -32,18 +27,16 @@ uint64_t nPrime(uint64_t n) {
 }
 
 uint64_t nextPrime(uint64_t value) {
-	int i = 1;
-	while (!checkPrime(value + i))
-	{
+	uint64_t i = 1;
+	while (!checkPrime(value + i)) {
 		i++;
 	}
 	return value + i;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-	int psum = 0;
-	for (int i = 2; i < hbound; i++)
-	{
+	uint64_t psum = 0;
+	for (int i = 2; i < hbound; i++) {
 		if (checkPrime(i))
 			psum += i;
 	}
@@ -51,11 +44,10 @@ uint64_t sumPrime(uint64_t hbound) {
 }
 
 uint64_t twinPrimes(uint64_t lbound, uint64_t hbound) {
-	int twinp_count = 0;
-	for (int i = lbound; i < hbound; i++)
-	{
-		if (checkPrime(i) && checkPrime(i+2) && (i+2) < hbound)
-			twinp_count++;
+	uint64_t twinp_count = 0;
+	for (int i = lbound; i < hbound; i++) {
+		if (checkPrime(i) && checkPrime(i+2) && (i+2) < hbound) 
+      twinp_count++;
 	}
 	return twinp_count;
 }
